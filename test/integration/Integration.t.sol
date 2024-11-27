6f22ff5af1f739920146afe1d55633a7ede18d79
rambo <jpramboguanabara@hotmail.com>
2024-11-27 16:13:24 -0300

    feat: start integration test

--- test/integration/Integration.t.sol
+++ test/integration/Integration.t.sol
@@ -2,3 +2,29 @@
 // integration
 // forked
 // staging
+
+// fuzzing
+// stateful fuzz
+// stateless fuzz
+// formal verification
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import {Test} from "forge-std/Test.sol";
+import {Raffle} from "src/Raffle.sol";
+import {DeployRaffle} from "script/DeployRaffle.s.sol";
+
+contract InteractionsTest is Test {
+    Raffle raffle;
+    address PLAYER = makeAddr("player");
+
+    function setUp() external {
+        DeployRaffle deployRaffle = new DeployRaffle();
+        raffle = deployRaffle.run();
+        vm.deal(PLAYER, 1 ether);
+    }
+
+    function testPlayerCanJoinRaffle() external {
+        // todo
+    }
+}
